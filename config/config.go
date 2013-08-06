fcf8cc3fb5100a2466c414cea91a4bb6344087fd
Cong Ding <dinggnu@gmail.com>
2013-08-07 00:40:58 +0200

    fix bug in checkComment

--- config/config.go
+++ config/config.go
@@ -89,6 +89,7 @@ func checkLine(line string) (string, string, error) {
 }
 
 func checkComment(line string) bool {
+	line = strings.TrimSpace(line)
 	for p := range commentPrefix {
 		if strings.HasPrefix(line, commentPrefix[p]) {
 			return true
